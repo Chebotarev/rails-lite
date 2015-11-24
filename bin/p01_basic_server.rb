1dd2cd0f45f0c1d57da5da5b30043a241e08db8c
Alex Chebotarev <aleksey.chebotarev@gmail.com>
2015-11-23 20:13:34 -0800

    Finish Phase 1

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -1,1 +1,15 @@
+#!/usr/bin/env ruby
 require 'rack'
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/json'
+  res.write(env)
+  res.finish
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
